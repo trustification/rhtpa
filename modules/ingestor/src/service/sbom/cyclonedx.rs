2c357b0c0fc69020405ab29809c4ae53ccf6d061
desmax74 <mdessi@redhat.com>
2025-09-25 15:05:43 +0200

    feat: Add support for aibom and cbom detection
    This PR loops through the sbom components and applies kind label if it finds ai or crypto components inside
    
    Signed-off-by: desmax74 <mdessi@redhat.com>

--- modules/ingestor/src/service/sbom/cyclonedx.rs
+++ modules/ingestor/src/service/sbom/cyclonedx.rs
@@ -4,6 +4,8 @@ use crate::{
     service::{Error, Warnings},
 };
 use sea_orm::TransactionTrait;
+use serde_cyclonedx::cyclonedx::v_1_6::Component;
+use std::str::FromStr;
 use tracing::instrument;
 use trustify_common::{hashing::Digests, id::Id};
 use trustify_entity::labels::Labels;
@@ -29,7 +31,7 @@ impl<'g> CyclonedxLoader<'g> {
         let cdx: Box<serde_cyclonedx::cyclonedx::v_1_6::CycloneDx> = serde_json::from_slice(buffer)
             .map_err(|err| Error::UnsupportedFormat(format!("Failed to parse: {err}")))?;
 
-        let labels = labels.add("type", "cyclonedx");
+        let labels_updated = extract_labels(cdx.components.as_ref(), labels);
 
         log::info!(
             "Storing - version: {:?}, serialNumber: {:?}",
@@ -50,7 +52,7 @@ impl<'g> CyclonedxLoader<'g> {
         let ctx = match self
             .graph
             .ingest_sbom(
-                labels,
+                labels_updated,
                 digests,
                 document_id.clone(),
                 cyclonedx::Information(&cdx),
@@ -75,6 +77,50 @@ impl<'g> CyclonedxLoader<'g> {
     }
 }
 
+enum Kind {
+    AIBom,
+    CBom,
+}
+
+impl Kind {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Kind::AIBom => "aibom",
+            Kind::CBom => "cbom",
+        }
+    }
+}
+
+impl FromStr for Kind {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "machine-learning-model" => Ok(Kind::AIBom),
+            "cryptographic-asset" => Ok(Kind::CBom),
+            _ => Err(()),
+        }
+    }
+}
+
+fn extract_labels(components: Option<&Vec<Component>>, labels_in: Labels) -> Labels {
+    let mut labels = Labels::new().add("type", "cyclonedx");
+
+    if let Some(components) = components {
+        for component in components {
+            if let Ok(kind) = Kind::from_str(&component.type_) {
+                labels = labels.add("kind", kind.as_str());
+            }
+        }
+    }
+
+    if !labels_in.is_empty() {
+        return labels.extend(labels_in.0.clone());
+    }
+
+    labels
+}
+
 #[cfg(test)]
 mod test {
     use crate::service::{Cache, IngestorService};
@@ -97,6 +143,76 @@ mod test {
                 &data,
                 Format::CycloneDX,
                 ("source", "test"),
+                None,
+                Cache::Skip,
+            )
+            .await
+            .expect("must ingest");
+
+        Ok(())
+    }
+
+    #[test_context(TrustifyContext)]
+    #[test(tokio::test)]
+    async fn ingest_ai_cyclonedx_nvidia(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
+        let db = &ctx.db;
+        let graph = Graph::new(db.clone());
+        let data = document_bytes("cyclonedx/ai/nvidia_canary-1b-v2_aibom.json").await?;
+
+        let ingestor = IngestorService::new(graph, ctx.storage.clone(), Default::default());
+
+        ingestor
+            .ingest(
+                &data,
+                Format::CycloneDX,
+                [("type", "cyclonedx"), ("kind", "aibom")],
+                None,
+                Cache::Skip,
+            )
+            .await
+            .expect("must ingest");
+
+        Ok(())
+    }
+
+    #[test_context(TrustifyContext)]
+    #[test(tokio::test)]
+    async fn ingest_ai_cyclonedx_ibm(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
+        let db = &ctx.db;
+        let graph = Graph::new(db.clone());
+        let data =
+            document_bytes("cyclonedx/ai/ibm-granite_granite-docling-258M_aibom.json").await?;
+
+        let ingestor = IngestorService::new(graph, ctx.storage.clone(), Default::default());
+
+        ingestor
+            .ingest(
+                &data,
+                Format::CycloneDX,
+                [("type", "cyclonedx"), ("kind", "aibom")],
+                None,
+                Cache::Skip,
+            )
+            .await
+            .expect("must ingest");
+
+        Ok(())
+    }
+
+    #[test_context(TrustifyContext)]
+    #[test(tokio::test)]
+    async fn ingest_cryptographic_cyclonedx(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
+        let db = &ctx.db;
+        let graph = Graph::new(db.clone());
+        let data = document_bytes("cyclonedx/cryptographic/cbom.json").await?;
+
+        let ingestor = IngestorService::new(graph, ctx.storage.clone(), Default::default());
+
+        ingestor
+            .ingest(
+                &data,
+                Format::CycloneDX,
+                [("type", "cyclonedx"), ("kind", "cbom")],
                 None,
                 Cache::Skip,
             )
