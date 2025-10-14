cee53717d0d7443e2620ceef27065534f348d98b
Dejan Bosanac <dbosanac@redhat.com>
2025-10-14 11:18:31 +0200

    feat: use cvss library to parse cve scores
    Signed-off-by: Dejan Bosanac <dbosanac@redhat.com>

--- modules/ingestor/src/service/advisory/cve/mod.rs
+++ modules/ingestor/src/service/advisory/cve/mod.rs
@@ -1,26 +1,52 @@
 use crate::graph::cvss::ScoreCreator;
 use cve::Cve;
+use cvss::{Cvss, v2_0::CvssV2, v3::CvssV3, v4_0::CvssV4};
 
 pub mod divination;
 pub mod loader;
 
-pub fn extract_scores(cve: &Cve, creator: &mut ScoreCreator) {
-    let Cve::Published(cve) = cve else {
+pub fn extract_scores(cve: &Cve, _score_creator: &mut ScoreCreator) {
+    let Cve::Published(published) = cve else {
         return;
     };
 
-    for metrics in &cve.containers.cna.metrics {
-        if let Some(value) = &metrics.cvss_v2_0 {
-            // TODO: add score to creator
-        }
-        if let Some(value) = &metrics.cvss_v3_0 {
-            // TODO: add score to creator
-        }
-        if let Some(value) = &metrics.cvss_v3_1 {
-            // TODO: add score to creator
-        }
-        if let Some(value) = &metrics.cvss_v4_0 {
-            // TODO: add score to creator
+    let all_metrics = published.containers.cna.metrics.iter().chain(
+        published
+            .containers
+            .adp
+            .iter()
+            .flat_map(|adp| adp.metrics.iter()),
+    );
+
+    for metric in all_metrics {
+        let cvss_objects: Vec<Cvss> = [
+            metric.cvss_v2_0.as_ref().and_then(|v| {
+                serde_json::from_value::<CvssV2>(v.clone())
+                    .ok()
+                    .map(Cvss::V2)
+            }),
+            metric.cvss_v3_0.as_ref().and_then(|v| {
+                serde_json::from_value::<CvssV3>(v.clone())
+                    .ok()
+                    .map(Cvss::V3_0)
+            }),
+            metric.cvss_v3_1.as_ref().and_then(|v| {
+                serde_json::from_value::<CvssV3>(v.clone())
+                    .ok()
+                    .map(Cvss::V3_1)
+            }),
+            metric.cvss_v4_0.as_ref().and_then(|v| {
+                serde_json::from_value::<CvssV4>(v.clone())
+                    .ok()
+                    .map(Cvss::V4)
+            }),
+        ]
+        .into_iter()
+        .flatten()
+        .collect();
+
+        for cvss in cvss_objects {
+            println!("Parsed CVSS: {:?}", cvss);
         }
     }
 }
