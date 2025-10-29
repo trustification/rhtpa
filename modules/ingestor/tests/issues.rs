92339a27e3b9e75aeeef289d37ce80add81da52f
Jens Reimann <ctron@dentrassi.de>
2025-10-29 16:29:54 +0100

    test: the cvss score parsing should no longer fail

--- modules/ingestor/tests/issues.rs
+++ modules/ingestor/tests/issues.rs
@@ -6,7 +6,7 @@ use trustify_common::id::Id;
 use trustify_test_context::TrustifyContext;
 
 #[test_context(TrustifyContext)]
-#[test(actix_web::test)]
+#[test(tokio::test)]
 /// Ingested SBOM should not fail
 async fn issue_1492(ctx: &TrustifyContext) -> anyhow::Result<()> {
     let result = ctx
@@ -19,17 +19,14 @@ async fn issue_1492(ctx: &TrustifyContext) -> anyhow::Result<()> {
 }
 
 #[test_context(TrustifyContext)]
-#[test(actix_web::test)]
+#[test(tokio::test)]
 /// Ingested SBOM should not fail
 async fn cvss_issue_1(ctx: &TrustifyContext) -> anyhow::Result<()> {
     let result = ctx
         .ingest_document("csaf/issues/cvss_1/ssa-054046.json")
-        .await;
+        .await?;
 
-    assert_eq!(
-        result.expect_err("must be an error").to_string(),
-        "unknown CVSS metric name: `E` at line 3001 column 11"
-    );
+    assert!(matches!(result.id, Id::Uuid(_)));
 
     Ok(())
 }
