7d941ac7e8f8692df0d6ca52a4e2abba253fb1cd
Jens Reimann <ctron@dentrassi.de>
2025-10-29 13:24:19 +0100

    test: fix up oom test
    First we need to ensure `teardown` is being run. This can be done by
    dropping the `skip_teardown` attribute.
    
    Second, we need to refresh the ID. Adding the SQL statement used for
    future reference.
    
    Third, add some more explanation about the expectation towards the test.

--- modules/fundamental/tests/issues/oom.rs
+++ modules/fundamental/tests/issues/oom.rs
@@ -2,22 +2,47 @@
 
 use std::str::FromStr;
 use test_context::test_context;
+use test_log::test;
 use trustify_common::id::Id;
 use trustify_module_fundamental::sbom::service::SbomService;
-use trustify_test_context::{TrustifyContext, flame::setup_global_subscriber};
+use trustify_test_context::TrustifyContext;
 
-#[test_context(TrustifyContext, skip_teardown)]
-#[tokio::test]
+#[test_context(TrustifyContext)]
+#[test(tokio::test)]
 #[ignore = "Only works with a pre-existing database and a specific dump"]
-async fn fetch(ctx: TrustifyContext) -> anyhow::Result<()> {
-    let _guard = setup_global_subscriber();
-
+/// This test will not fail, but running with `--test-threads=1` it will show the amount of memory
+/// used, using [`TrustifyTestContext::teardown`].
+async fn fetch(ctx: &TrustifyContext) -> anyhow::Result<()> {
     // this requires an imported dataset
 
     let service = SbomService::new(ctx.db.clone());
     // update this digest to point to a "large SBOM"
+    // the following statement can be used"
+    /*
+    select
+        e.sha256
+    from
+        sbom_package_purl_ref a
+            join qualified_purl b on b.id = a.qualified_purl_id
+            join versioned_purl c on c.id = b.versioned_purl_id
+            join sbom d on d.sbom_id = a.sbom_id
+            join source_document e on d.source_document_id = e.id
+    where
+        c.base_purl_id = (
+            select a.base_purl_id
+            from purl_status a
+            group by a.base_purl_id
+            order by count(*) desc
+            limit 1
+        )
+    group by
+        e.sha256
+    order by
+        count(*) desc
+    limit 1;
+         */
     let id =
-        Id::from_str("sha256:f293eb898192085804419f9dd40a738f20d67dd81846e88c6720f692ec5f3081")?;
+        Id::from_str("sha256:a08f4d8723d3f2e1e12ba4a8961c6ebccfd603577d784b24576c09be8925af40")?;
     let statuses: Vec<String> = vec!["affected".to_string()];
 
     let result = service.fetch_sbom_details(id, statuses, &ctx.db).await?;
