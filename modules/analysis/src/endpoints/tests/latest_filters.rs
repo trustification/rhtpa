3ab479ed93ca6d01de9eadb01ed672c3ef8a166f
JimFuller-RedHat <jfuller@redhat.com>
2026-01-07 12:49:41 +0100

    latest- we are changing expected result from 1 to 2 due to work done in PR #2171 ... this test change is with q= only (which is not on critical path).

--- modules/analysis/src/endpoints/tests/latest_filters.rs
+++ modules/analysis/src/endpoints/tests/latest_filters.rs
@@ -22,7 +22,7 @@ use trustify_test_context::{TrustifyContext, subset::ContainsSubset};
 )]
 #[case( // purl q latest
     Req { what: What::Q("pkg:oci/quay-builder-qemu-rhcos-rhel8"), ancestors: Some(10), latest: true, ..Req::default() },
-    1
+    2
 )]
 #[case( // purl partial search
     Req { what: What::Q("purl~pkg:oci/quay-builder-qemu-rhcos-rhel8"), ancestors: Some(10), ..Req::default() },
