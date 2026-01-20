trustification/trustify

chore: initial PoC impl

migration/src/lib.rs
@@ -1,5 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
+mod data;
+
 mod m0000010_init;
 mod m0000020_add_sbom_group;
 mod m0000030_perf_adv_vuln;
@@ -25,6 +27,7 @@ mod m0001100_remove_get_purl;
 mod m0001110_sbom_node_checksum_indexes;
 mod m0001120_sbom_external_node_indexes;
 mod m0001130_gover_cmp;
+mod m0001140_example_data_migration;
 
 pub struct Migrator;
 
@@ -57,6 +60,7 @@ impl MigratorTrait for Migrator {
             Box::new(m0001110_sbom_node_checksum_indexes::Migration),
             Box::new(m0001120_sbom_external_node_indexes::Migration),
             Box::new(m0001130_gover_cmp::Migration),
+            Box::new(m0001140_example_data_migration::Migration),
         ]
     }
 }

trustification/rhtpa

migration/src/lib.rs
@@ -1,5 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
+mod data;
+
 mod m0000010_init;
 mod m0000020_add_sbom_group;
 mod m0000030_perf_adv_vuln;
@@ -32,6 +34,7 @@ mod m0001170_non_null_source_document_id;
 mod m0001180_expand_spdx_licenses_with_mappings_function;
 mod m0001190_optimize_product_advisory_query;
 mod m0001200_source_document_fk_indexes;
+mod m0002000_example_data_migration;
 
 pub struct Migrator;
 
@@ -71,6 +74,7 @@ impl MigratorTrait for Migrator {
             Box::new(m0001180_expand_spdx_licenses_with_mappings_function::Migration),
             Box::new(m0001190_optimize_product_advisory_query::Migration),
             Box::new(m0001200_source_document_fk_indexes::Migration),
+            Box::new(m0002000_example_data_migration::Migration),
         ]
     }
 }