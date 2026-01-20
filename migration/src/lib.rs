ctron/trustify

feat: allow running data migrations as part of migrations

migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod data;
+use crate::data::MigrationWithData;
 
 mod m0000010_init;
 mod m0000020_add_sbom_group;
@@ -60,7 +61,9 @@ impl MigratorTrait for Migrator {
             Box::new(m0001110_sbom_node_checksum_indexes::Migration),
             Box::new(m0001120_sbom_external_node_indexes::Migration),
             Box::new(m0001130_gover_cmp::Migration),
-            Box::new(m0001140_example_data_migration::Migration),
+            Box::new(MigrationWithData::new(
+                m0001140_example_data_migration::Migration,
+            )),
         ]
     }
 }

trustification/rhtpa

migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod data;
+use crate::data::MigrationWithData;
 
 mod m0000010_init;
 mod m0000020_add_sbom_group;
@@ -74,7 +75,9 @@ impl MigratorTrait for Migrator {
             Box::new(m0001180_expand_spdx_licenses_with_mappings_function::Migration),
             Box::new(m0001190_optimize_product_advisory_query::Migration),
             Box::new(m0001200_source_document_fk_indexes::Migration),
-            Box::new(m0002000_example_data_migration::Migration),
+            Box::new(MigrationWithData::new(
+                m0002000_example_data_migration::Migration,
+            )),
         ]
     }
 }