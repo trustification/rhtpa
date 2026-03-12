mrizzi/trustify

perf(license): add indexes and refactor 'NOT EXIST' query (TC-3591)
Signed-off-by: mrizzi <mrizzi@redhat.com>
Assisted-by: Claude Code

migration/src/lib.rs
@@ -40,6 +40,7 @@ mod m0001200_source_document_fk_indexes;
 mod m0002000_add_sbom_properties;
 mod m0002010_add_advisory_scores;
 mod m0002020_add_sbom_group;
+mod m0002030_license_query_performance;
 
 pub trait MigratorExt: Send {
     fn build_migrations() -> Migrations;
@@ -96,6 +97,7 @@ impl MigratorExt for Migrator {
             .data(m0002000_add_sbom_properties::Migration)
             .data(m0002010_add_advisory_scores::Migration)
             .normal(m0002020_add_sbom_group::Migration)
+            .normal(m0002030_license_query_performance::Migration)
     }
 }
 

trustification/rhtpa

migration/src/lib.rs
@@ -49,6 +49,7 @@ mod m0002070_alter_sbom_group_restrict_parent;
 mod m0002080_add_cargo_version_scheme;
 mod m0002090_vulnerability_id_sort_index;
 mod m0002100_analysis_perf_indexes;
+mod m0002110_license_query_performance;
 
 pub trait MigratorExt: Send {
     fn build_migrations() -> Migrations;
@@ -114,6 +115,7 @@ impl MigratorExt for Migrator {
             .normal(m0002080_add_cargo_version_scheme::Migration)
             .normal(m0002090_vulnerability_id_sort_index::Migration)
             .normal(m0002100_analysis_perf_indexes::Migration)
+            .normal(m0002110_license_query_performance::Migration)
     }
 }
 