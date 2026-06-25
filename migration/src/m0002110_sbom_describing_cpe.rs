guacsec/trustify

refactor: try to get more speed by materializing describing CPEs
Co-authored-by: Claude <noreply@anthropic.com>

migration/src/m0002110_sbom_describing_cpe.rs
@@ -1,0 +1,92 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Create the sbom_describing_cpe table to materialize the CPEs associated
+        // with packages that describe each SBOM, avoiding repeated joins at query time.
+        manager
+            .create_table(
+                Table::create()
+                    .table(SbomDescribingCpe::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(SbomDescribingCpe::SbomId).uuid().not_null())
+                    .col(ColumnDef::new(SbomDescribingCpe::CpeId).uuid().not_null())
+                    .primary_key(
+                        Index::create()
+                            .col(SbomDescribingCpe::SbomId)
+                            .col(SbomDescribingCpe::CpeId),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(SbomDescribingCpe::Table, SbomDescribingCpe::SbomId)
+                            .to(Sbom::Table, Sbom::SbomId)
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(SbomDescribingCpe::Table, SbomDescribingCpe::CpeId)
+                            .to(Cpe::Table, Cpe::Id),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        // Backfill from existing data: find CPEs on nodes involved in Describes relationships.
+        // The OR covers both patterns (CPE on right_node_id for SPDX/CycloneDX,
+        // and CPE on left_node_id for the ingest_describes_cpe22 helper).
+        manager
+            .get_connection()
+            .execute_unprepared(
+                r#"
+                INSERT INTO sbom_describing_cpe (sbom_id, cpe_id)
+                SELECT DISTINCT spcr.sbom_id, spcr.cpe_id
+                FROM sbom_package_cpe_ref spcr
+                JOIN package_relates_to_package prtp
+                  ON prtp.sbom_id = spcr.sbom_id
+                 AND (prtp.right_node_id = spcr.node_id OR prtp.left_node_id = spcr.node_id)
+                WHERE prtp.relationship = 13
+                ON CONFLICT DO NOTHING
+                "#,
+            )
+            .await
+            .map(|_| ())?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(
+                Table::drop()
+                    .if_exists()
+                    .table(SbomDescribingCpe::Table)
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+pub enum SbomDescribingCpe {
+    Table,
+    SbomId,
+    CpeId,
+}
+
+#[derive(DeriveIden)]
+pub enum Sbom {
+    Table,
+    SbomId,
+}
+
+#[derive(DeriveIden)]
+pub enum Cpe {
+    Table,
+    Id,
+}

modules/ingestor/src/graph/sbom/mod.rs
@@ -22,8 +22,9 @@ use crate::{
 use cpe::uri::OwnedUri;
 use entity::{product, product_version};
 use sea_orm::{
-    ActiveModelTrait, ColumnTrait, ConnectionTrait, EntityTrait, ModelTrait, QueryFilter,
-    QuerySelect, RelationTrait, Select, Set, TransactionTrait, prelude::Uuid,
+    ActiveModelTrait, ColumnTrait, ConnectionTrait, DbBackend, EntityTrait, ModelTrait,
+    QueryFilter, QuerySelect, RelationTrait, Select, Set, Statement, TransactionTrait,
+    prelude::Uuid,
 };
 use sea_query::{Condition, Expr, Func, JoinType, Query, SimpleExpr, extension::postgres::PgExpr};
 use std::{
@@ -583,6 +584,30 @@ impl SbomContext {
             connection,
         )
         .await?;
+        Ok(())
+    }
+
+    /// Materializes describing CPE associations from the relationship and CPE ref
+    /// data already inserted for this SBOM into the `sbom_describing_cpe` table.
+    pub async fn populate_describing_cpes<C: ConnectionTrait>(&self, db: &C) -> Result<(), Error> {
+        db.execute(Statement::from_sql_and_values(
+            DbBackend::Postgres,
+            r#"
+            INSERT INTO sbom_describing_cpe (sbom_id, cpe_id)
+            SELECT DISTINCT spcr.sbom_id, spcr.cpe_id
+            FROM sbom_package_cpe_ref spcr
+            JOIN package_relates_to_package prtp
+              ON prtp.sbom_id = spcr.sbom_id
+             AND (prtp.right_node_id = spcr.node_id OR prtp.left_node_id = spcr.node_id)
+            WHERE prtp.sbom_id = $1
+              AND prtp.relationship = 13
+            ON CONFLICT DO NOTHING
+            "#,
+            [self.sbom.sbom_id.into()],
+        ))
+        .await
+        .map_err(Error::Database)?;
+
         Ok(())
     }
 

trustification/rhtpa

migration/src/m0002110_sbom_describing_cpe.rs
@@ -1,0 +1,92 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Create the sbom_describing_cpe table to materialize the CPEs associated
+        // with packages that describe each SBOM, avoiding repeated joins at query time.
+        manager
+            .create_table(
+                Table::create()
+                    .table(SbomDescribingCpe::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(SbomDescribingCpe::SbomId).uuid().not_null())
+                    .col(ColumnDef::new(SbomDescribingCpe::CpeId).uuid().not_null())
+                    .primary_key(
+                        Index::create()
+                            .col(SbomDescribingCpe::SbomId)
+                            .col(SbomDescribingCpe::CpeId),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(SbomDescribingCpe::Table, SbomDescribingCpe::SbomId)
+                            .to(Sbom::Table, Sbom::SbomId)
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(SbomDescribingCpe::Table, SbomDescribingCpe::CpeId)
+                            .to(Cpe::Table, Cpe::Id),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        // Backfill from existing data: find CPEs on nodes involved in Describes relationships.
+        // The OR covers both patterns (CPE on right_node_id for SPDX/CycloneDX,
+        // and CPE on left_node_id for the ingest_describes_cpe22 helper).
+        manager
+            .get_connection()
+            .execute_unprepared(
+                r#"
+                INSERT INTO sbom_describing_cpe (sbom_id, cpe_id)
+                SELECT DISTINCT spcr.sbom_id, spcr.cpe_id
+                FROM sbom_node_cpe_ref spcr
+                JOIN package_relates_to_package prtp
+                  ON prtp.sbom_id = spcr.sbom_id
+                 AND (prtp.right_node_id = spcr.node_id OR prtp.left_node_id = spcr.node_id)
+                WHERE prtp.relationship = 13
+                ON CONFLICT DO NOTHING
+                "#,
+            )
+            .await
+            .map(|_| ())?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(
+                Table::drop()
+                    .if_exists()
+                    .table(SbomDescribingCpe::Table)
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+pub enum SbomDescribingCpe {
+    Table,
+    SbomId,
+    CpeId,
+}
+
+#[derive(DeriveIden)]
+pub enum Sbom {
+    Table,
+    SbomId,
+}
+
+#[derive(DeriveIden)]
+pub enum Cpe {
+    Table,
+    Id,
+}

modules/ingestor/src/graph/sbom/mod.rs
@@ -21,8 +21,9 @@ use crate::{
 use cpe::uri::OwnedUri;
 use entity::{product, product_version};
 use sea_orm::{
-    ActiveModelTrait, ColumnTrait, ConnectionTrait, EntityTrait, ModelTrait, QueryFilter,
-    QuerySelect, RelationTrait, Select, Set, TransactionTrait, prelude::Uuid,
+    ActiveModelTrait, ColumnTrait, ConnectionTrait, DbBackend, EntityTrait, ModelTrait,
+    QueryFilter, QuerySelect, RelationTrait, Select, Set, Statement, TransactionTrait,
+    prelude::Uuid,
 };
 use sea_query::{Condition, Expr, Func, JoinType, Query, SimpleExpr, extension::postgres::PgExpr};
 use std::{
@@ -581,6 +582,30 @@ impl SbomContext {
             connection,
         )
         .await?;
+        Ok(())
+    }
+
+    /// Materializes describing CPE associations from the relationship and CPE ref
+    /// data already inserted for this SBOM into the `sbom_describing_cpe` table.
+    pub async fn populate_describing_cpes<C: ConnectionTrait>(&self, db: &C) -> Result<(), Error> {
+        db.execute(Statement::from_sql_and_values(
+            DbBackend::Postgres,
+            r#"
+            INSERT INTO sbom_describing_cpe (sbom_id, cpe_id)
+            SELECT DISTINCT spcr.sbom_id, spcr.cpe_id
+            FROM sbom_node_cpe_ref spcr
+            JOIN package_relates_to_package prtp
+              ON prtp.sbom_id = spcr.sbom_id
+             AND (prtp.right_node_id = spcr.node_id OR prtp.left_node_id = spcr.node_id)
+            WHERE prtp.sbom_id = $1
+              AND prtp.relationship = 13
+            ON CONFLICT DO NOTHING
+            "#,
+            [self.sbom.sbom_id.into()],
+        ))
+        .await
+        .map_err(Error::Database)?;
+
         Ok(())
     }
 