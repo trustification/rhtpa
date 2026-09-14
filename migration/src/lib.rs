guacsec/trustify

fix(migration): mark pythonver_cmp PARALLEL RESTRICTED
pythonver_cmp was declared IMMUTABLE PARALLEL SAFE while containing a
WHEN OTHERS exception handler. Entering a PL/pgSQL exception block starts
an internal subtransaction which PostgreSQL forbids inside parallel
worker ('cannot start subtransactions during a parallel operation').

Once tables grow past  parallel-scan thresholds after imports the
planner pushes pythonver_cmp (reached via version_matches on the PyPI
path) below a Gather node during SBOM vulnerability analysis, aborting the
query and breaking the SBOM view. This surfaced on PostgreSQL 18, whose
optimizer picks parallel plans more readily than 17.

Relabel function PARALLEL RESTRICTED so it runs only in the leader
where subtransactions are permitted while leaving the rest of each plan
parallelizable. Function body unchanged.

Fixes TC-5645

migration/src/lib.rs
@@ -73,6 +73,7 @@ mod m0002280_backfill_sbom_suppliers;
 mod m0002290_create_exploit_intelligence_job;
 mod m0002300_create_exploit;
 mod m0002310_create_change_log;
+mod m0002320_pythonver_cmp_parallel_restricted;
 
 pub trait MigratorExt: Send {
     fn build_migrations() -> Migrations;
@@ -161,6 +162,7 @@ impl MigratorExt for Migrator {
             .normal(m0002290_create_exploit_intelligence_job::Migration)
             .normal(m0002300_create_exploit::Migration)
             .normal(m0002310_create_change_log::Migration)
+            .normal(m0002320_pythonver_cmp_parallel_restricted::Migration)
     }
 }
 

trustification/rhtpa

migration/src/lib.rs
@@ -79,6 +79,7 @@ mod m0002320_fix_unbounded_version_matches;
 mod m0002330_fix_rpmver_cmp;
 mod m0002340_backfill_rpm_epoch;
 mod m0002350_drop_purl_status_gist_index;
+mod m0002360_pythonver_cmp_parallel_restricted;
 
 pub trait MigratorExt: Send {
     fn build_migrations() -> Migrations;
@@ -171,6 +172,7 @@ impl MigratorExt for Migrator {
             .normal(m0002330_fix_rpmver_cmp::Migration)
             .normal(m0002340_backfill_rpm_epoch::Migration)
             .normal(m0002350_drop_purl_status_gist_index::Migration)
+            .normal(m0002360_pythonver_cmp_parallel_restricted::Migration)
     }
 }
 